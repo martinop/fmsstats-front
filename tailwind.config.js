4d3848c268a15738022a28251e5cadc904ea27e1
Martin Ocando <martinocandop@gmail.com>
2020-07-07 20:37:46 -0400

    Initial Commit with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: [
+      './src/**/*.js',
+      './public/**/*.html',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
